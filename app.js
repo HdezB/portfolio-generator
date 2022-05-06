05ad5416678c4e54b835039ce9721f91e298df45
DukeHdez <briayan27@outlook.com>
2022-05-06 19:44:55 -0400

    Generate the HTML file

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = profileDataArr => {
 
-    //This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-    console.log('=============');
-
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete!');
+})
