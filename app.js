edb854aeb5dde0c48ad9376dbb5f454a36a86d7b
DukeHdez <briayan27@outlook.com>
2022-05-07 22:13:40 -0400

    Add the inquirer package

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+
+// const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHtml, err => {
+//     if (err) throw new Error(err);
 
-    console.log('Portfolio complete!');
-})
+//     console.log('Portfolio complete!');
+// })
+
