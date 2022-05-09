46ac0f764f7672f38d9c55e1aad71c8317356e8c
DukeHdez <briayan27@outlook.com>
2022-05-09 17:30:28 -0400

    Generate entire website

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
+
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -146,14 +147,20 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        const pageHtml = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHtml, err => {
-            if (err) throw new Error(err);
-
-            console.log('Page created! Check out index.html in this directory to see it!');
-
-        });
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
